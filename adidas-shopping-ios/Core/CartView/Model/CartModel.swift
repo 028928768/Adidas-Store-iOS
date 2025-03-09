62a49505827df0c9214905bb5ca47dcf55beed73
Simon Siwell <Kantachat_2539@hotmail.com>
2025-03-09 11:21:57 -0700

    [adidas-001] -  Cart Model prepared

---
+++ adidas-shopping-ios/Core/CartView/Model/CartModel.swift
@@ -0,0 +1,8 @@
+//
+//  CartModel.swift
+//  adidas-shopping-ios
+//
+//  Created by Simon SIwell on 9/3/2568 BE.
+//
+
+import Foundation
---
+++ adidas-shopping-ios/Core/HomeView/ProductDetail/ProductDetailVM.swift
@@ -0,0 +1,8 @@
+//
+//  ProductDetailVM.swift
+//  adidas-shopping-ios
+//
+//  Created by Simon SIwell on 9/3/2568 BE.
+//
+
+import Foundation
