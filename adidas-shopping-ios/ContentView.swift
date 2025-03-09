125c1f2b5b054407b6c662e47d384966dadc0394
Simon Siwell <Kantachat_2539@hotmail.com>
2025-03-08 23:56:25 -0800

    Initial Commit

---
+++ adidas-shopping-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  adidas-shopping-ios
+//
+//  Created by Simon SIwell on 8/3/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ adidas-shopping-ios/adidas_shopping_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  adidas_shopping_iosApp.swift
+//  adidas-shopping-ios
+//
+//  Created by Simon SIwell on 8/3/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct adidas_shopping_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
